26987c2fc5b861d5c63010ea54d116b210684f6d
andre <andrepg@ifsul.edu.br>
2024-07-08 16:55:36 -0300

    Primeira versão estática

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([
+        'Criar um projeto no firebase',
+        'Não esquecer de gravar as etapas'
+    ])
+    const [input, setInput] = useState('')
+    const addTodo = e => {
+        e.preventDefault()
+        setTodos([...todos, input])
+        setInput('')
+    }
+    return (
+      <div className="App">
+        <h1>TODO React Firestore</h1>
+          <form>
+              <input value={input} onChange={e => setInput(e.target.value)}/>
+              <button type="submit" onClick={addTodo}>Adicionar Todo</button>
+          </form>
+          <ul>
+              {todos.map(todo => <li>{todo}</li>)}
+          </ul>
+      </div>
+    );
 }
 
 export default App;
